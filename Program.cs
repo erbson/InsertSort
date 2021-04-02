da42c8e4d665919f92141520aca9b6c16ad12642
ERBSON LAVRAS DOS SANTOS <erbson.lavras@globaltec.com.br>
2021-04-02 20:09:02 -0300

    insertsort ordenação de vetor

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace InsertSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor = new int[10]  { 2, 5, -4, 11, 0, 18, 22, 67, 51, 6};
+
+            Console.WriteLine("Vetores não Ordenados");
+            mostraVetor(vetor);
+
+            Console.WriteLine("Vetores que Foram Ordenados");
+
+            mostraVetor(InsertSort(vetor));
+
+
+            Console.ReadKey();
+
+
+        }
+
+        private static int[] InsertSort(int[] vetor)
+        {
+
+            for (int i = 0; i < vetor.Length -1; i++)
+            {
+                for (int j = i +1; j>0; j--)
+                {
+
+                    if (vetor[j -1] > vetor[j])
+                    {
+                        int aux = vetor[j - 1];
+                        vetor[j - 1] = vetor[j];
+
+                        vetor[j] = aux;
+
+                   }
+                }
+            }
+
+
+            
+            return vetor;
+        }
+
+        private static void mostraVetor(int[] vetor)
+        {
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                Console.WriteLine(vetor[i]);
+
+
+            }
+        }
+    }
+}
